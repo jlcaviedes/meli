6a5454ee919f6430624af7f7ac24b7075a7cce74
Jorge Luis Caviedes <jlcaviedes1@gmail.com>
2022-05-02 23:52:37 -0500

    added files ignores interfaces, types others

--- jest.config.js
+++ jest.config.js
@@ -1,21 +1,24 @@
 // jest.config.js
-const { defaults } = require("jest-config");
+const { defaults } = require('jest-config');
 module.exports = {
-  verbose: true,
-  moduleFileExtensions: [...defaults.moduleFileExtensions, "ts", "tsx"],
-  collectCoverageFrom: [
-    "**/*.{js,jsx,ts}",
-    "!**/node_modules/**",
-    "!**/vendor/**",
-  ],
-  collectCoverage: true,
-  collectCoverageFrom: ["./src/**"],
-  coverageThreshold: {
-    global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: -10,
-    },
-  },
+	verbose: true,
+	moduleFileExtensions: [...defaults.moduleFileExtensions, 'ts', 'tsx'],
+	collectCoverageFrom: [
+		'**/*.{js,jsx,ts}',
+		'!src/**/*.interface.ts',
+		'!src/modules/products/entities/types/*.ts',
+		'!src/**/index.ts',
+		'!**/node_modules/**',
+		'!**/vendor/**',
+	],
+	collectCoverage: true,
+	collectCoverageFrom: ['./src/**'],
+	coverageThreshold: {
+		global: {
+			branches: 80,
+			functions: 80,
+			lines: 80,
+			statements: -10,
+		},
+	},
 };
