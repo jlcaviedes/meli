63803256c4a91b257d6a0b434d0060fe390a204f
Jorge Luis Caviedes <jlcaviedes1@gmail.com>
2022-05-02 23:52:37 -0500

    added files ignores interfaces, types others

--- src/modules/products/entities/interfaces/index.ts
+++ src/modules/products/entities/interfaces/index.ts
@@ -1,5 +1,5 @@
 export * from "./controller-api.interface";
 export * from "./controller-view.interface";
 export * from "./product-application.interface";
-export * from "./product-repository.interfaces";
+export * from "./product-repository.interface";
 export * from "./product-view.interface";
